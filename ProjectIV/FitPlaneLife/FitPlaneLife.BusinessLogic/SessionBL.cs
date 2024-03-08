083d1b2684be4bc89c0bf902527c4f6bd074ad49
Ruslan <ruslan.afanasiev@isa.utm.md>
2024-03-08 12:54:14 +0200

    Am adaugat BusinessLogic, Helpers si Domain

---
+++ ProjectIV/FitPlaneLife/FitPlaneLife.BusinessLogic/SessionBL.cs
@@ -0,0 +1,15 @@
+﻿using FitPlaneLife.BusinessLogic.Core;
+using FitPlaneLife.BusinessLogic.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FitPlaneLife.BusinessLogic
+{
+     public class SessionBL : UserApi, ISession
+     {
+
+     }
+}
--- ProjectIV/FitPlaneLife/FitPlaneLife/App_Start/BundleConfig.cs
+++ ProjectIV/FitPlaneLife/FitPlaneLife/App_Start/BundleConfig.cs
@@ -10,13 +10,33 @@ namespace FitPlaneLife.App_Start
     {
         public static void RegisterBundles(BundleCollection bundles)
         {
-               // Bootstrap style
-               bundles.Add(new StyleBundle("~/bundles/bootstrap/css").Include(
-                    "~/Content/bootstrap.min.css", new CssRewriteUrlTransform()));
+            
+               bundles.Add(new ScriptBundle("~/js").Include(
+                    "~/js/jquery-1.11.0.min.js",
+                    "~/js/bootstrap.min.js",
+                    "~/js/jquery.appear.js",
+                    "~/js/jquery.prettyPhoto.js",
+                    "~/js/modernizr-latest.js",
+                    "~/js/SmoothScroll.js",
+                    "~/js/jquery.parallax-1.1.3.js",
+                    "~/js/jquery.easing.1.3.js",
+                    "~/js/jquery.superslides.js",
+                    "~/js/jquery.flexslider.js",
+                    "~/js/jquery.mb.YTPlayer.js",
+                    "~/js/jquery.fitvids.js",
+                    "~/js/jquery.slabtext.js",
+                    "~/js/plugins.js"));
 
-               // Bootstrap
-               bundles.Add(new StyleBundle("~/bundles/bootstrap/js").Include(
-                   "~/Scripts/bootstrap.min.js"));
-          }
+               bundles.Add(new StyleBundle("~/css").Include(
+                    "~/css/reset.css",
+                    "~/css/animate.min.css",
+                    "~/css/bootstrap.min.css",
+                    "~/css/style.css",
+                    "~/css/font-awesome.css",
+                    "~/css/owl.carousel.css",
+                    "~/css/responsive.css",
+                    "~/css/player/YTPlayer.css",
+                    "~/css/pro-bars.css"));
+        }
     }
 }
