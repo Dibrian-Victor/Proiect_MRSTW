f8fe28f8e54a1923a41b23df4ad6d5f724f41a38
VictorD <dibrianvictor725@gmail.com>
2024-03-14 19:49:05 +0200

    Am modificat templateul

--- ProjectIV/FitPlaneLife/FitPlaneLife/App_Start/BundleConfig.cs
+++ ProjectIV/FitPlaneLife/FitPlaneLife/App_Start/BundleConfig.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Drawing;
 using System.Linq;
 using System.Web;
 using System.Web.Optimization;
@@ -10,33 +11,20 @@ namespace FitPlaneLife.App_Start
     {
         public static void RegisterBundles(BundleCollection bundles)
         {
-            
-               bundles.Add(new ScriptBundle("~/js").Include(
-                    "~/js/jquery-1.11.0.min.js",
-                    "~/js/bootstrap.min.js",
-                    "~/js/jquery.appear.js",
-                    "~/js/jquery.prettyPhoto.js",
-                    "~/js/modernizr-latest.js",
-                    "~/js/SmoothScroll.js",
-                    "~/js/jquery.parallax-1.1.3.js",
-                    "~/js/jquery.easing.1.3.js",
-                    "~/js/jquery.superslides.js",
-                    "~/js/jquery.flexslider.js",
-                    "~/js/jquery.mb.YTPlayer.js",
-                    "~/js/jquery.fitvids.js",
-                    "~/js/jquery.slabtext.js",
-                    "~/js/plugins.js"));
-
+    
                bundles.Add(new StyleBundle("~/css").Include(
-                    "~/css/reset.css",
-                    "~/css/animate.min.css",
-                    "~/css/bootstrap.min.css",
-                    "~/css/style.css",
-                    "~/css/font-awesome.css",
-                    "~/css/owl.carousel.css",
-                    "~/css/responsive.css",
-                    "~/css/player/YTPlayer.css",
-                    "~/css/pro-bars.css"));
+                    "~/Vendors/img/favicon.ico",
+                    "~/Vendors/lib/flaticon/font/flaticon.css",
+                    "~/Vendors/css/style.min.css"));
+
+               bundles.Add(new ScriptBundle("~/js").Include(
+                    "~/Vendors/lib/easing/easing.min.js",
+                    "~/Vendors/lib/waypoints/waypoints.min.js",
+                    "~/Vendors/mail/jqBootstrapValidation.min.js",
+                    "~/Vendors/mail/contact.js",
+                    "~/Vendors/js/main.js"));
         }
     }
 }
+
+   
