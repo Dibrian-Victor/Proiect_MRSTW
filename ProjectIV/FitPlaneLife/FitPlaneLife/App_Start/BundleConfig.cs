5185eeb434fe1be869e8a13f12ae6d209861412c
VictorD <dibrianvictor725@gmail.com>
2024-03-18 15:43:06 +0200

    Am adăugat un nou template

--- ProjectIV/FitPlaneLife/FitPlaneLife/App_Start/BundleConfig.cs
+++ ProjectIV/FitPlaneLife/FitPlaneLife/App_Start/BundleConfig.cs
@@ -10,33 +10,26 @@ namespace FitPlaneLife.App_Start
     {
         public static void RegisterBundles(BundleCollection bundles)
         {
-            
+               bundles.Add(new ScriptBundle("~/lib").Include(
+                    "~/lib/easing/easing.min.js",
+                    "~/lib/waypoints/waypoints.min.js",
+                    "~/lib/owlcarousel/owl.carousel.min.js",
+                    "~/lib/counterup/counterup.min.js"));
+
                bundles.Add(new ScriptBundle("~/js").Include(
-                    "~/js/jquery-1.11.0.min.js",
-                    "~/js/bootstrap.min.js",
-                    "~/js/jquery.appear.js",
-                    "~/js/jquery.prettyPhoto.js",
-                    "~/js/modernizr-latest.js",
-                    "~/js/SmoothScroll.js",
-                    "~/js/jquery.parallax-1.1.3.js",
-                    "~/js/jquery.easing.1.3.js",
-                    "~/js/jquery.superslides.js",
-                    "~/js/jquery.flexslider.js",
-                    "~/js/jquery.mb.YTPlayer.js",
-                    "~/js/jquery.fitvids.js",
-                    "~/js/jquery.slabtext.js",
-                    "~/js/plugins.js"));
+                   "~/js/main.js"));
+
+               bundles.Add(new StyleBundle("~/img").Include(
+                    "~/img/favicon.ico"));
 
                bundles.Add(new StyleBundle("~/css").Include(
-                    "~/css/reset.css",
-                    "~/css/animate.min.css",
                     "~/css/bootstrap.min.css",
-                    "~/css/style.css",
-                    "~/css/font-awesome.css",
-                    "~/css/owl.carousel.css",
-                    "~/css/responsive.css",
-                    "~/css/player/YTPlayer.css",
-                    "~/css/pro-bars.css"));
+                    "~/css/style.css"));
+
+               bundles.Add(new StyleBundle("~/lib").Include(
+                    "~/lib/owlcarousel/assets/owl.carousel.min.css",
+                    "~/lib/flaticon/font/flaticon.css"));
+
         }
     }
 }
